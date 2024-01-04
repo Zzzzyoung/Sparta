bae6e9a99b9a757a5a25a56bda4c3f5d58fd04f8
JIYOUNG <154482077+Zzzzyoung@users.noreply.github.com>
2024-01-04 12:31:53 +0900

    Delete [JavaScript] 문법 종합반/week4 directory

--- [JavaScript] 문법 종합반/week4/01.js
+++
@@ -1,9 +0,0 @@
-// 콜백 함수
-setTimeout(function() {
-    console.log('Hello');
-}, 1000);
-
-const numbers = [1, 2, 3, 4, 5];
-numbers.forEach(function(number) {
-    console.log(number);
-});
--- [JavaScript] 문법 종합반/week4/12.js
+++
@@ -1,9 +0,0 @@
-// 비동기적 코드의 이해
-setTimeout(function () {
-    console.log('여기가 먼저 실행될까?');
-}, 1000);
-
-console.log('여기 좀 봐주세요!');
-
-// setTimeour이 끝나는 것을 기다리지 않고, 주문만 미리 받아놓고, 밑에 console.log 실행
-// 위에 console.log는 1초가 지난 뒤에 실행
